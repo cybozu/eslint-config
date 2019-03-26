240a602e308d147054fcb718f6bc4d872453bece
toshi-toma <toshitoma1217@gmail.com>
2019-03-26 16:55:27 +0900

    add presets/react-typescript-prettier

---
+++ presets/react-typescript-prettier.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "../lib/base.js",
+    "../lib/react.js",
+    "../lib/typescript.js",
+    "../lib/prettier.js",
+    "../lib/prettier-react.js",
+    "../lib/prettier-typescript.js"
+  ]
+};
