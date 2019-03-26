a2a7d04f78452897da6efd49a995493b7f5b4acb
Toru Kobayashi <koba0004@gmail.com>
2019-03-26 16:39:37 +0900

    add preset/typescript-prettier and preset/typescript-node-prettier

---
+++ presets/typescript-node-prettier.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "../lib/base.js",
+    "../lib/node.js",
+    "../lib/typescript.js",
+    "../lib/node-typescript.js",
+    "../lib/prettier.js",
+    "../lib/prettier-typescript.js"
+  ]
+};
