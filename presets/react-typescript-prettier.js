33eba267894d24e587d61c47a616d28296e9c0fd
Toru Kobayashi <koba0004@gmail.com>
2019-03-27 16:32:00 +0900

    tweak presets and rules

--- presets/react-typescript-prettier.js
+++ presets/react-typescript-prettier.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     "../lib/base.js",
     "../lib/react.js",
+    "../lib/react-typescript.js",
     "../lib/typescript.js",
     "../lib/prettier.js",
     "../lib/prettier-react.js",
