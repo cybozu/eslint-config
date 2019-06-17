2439e2baabfeaf4b6876baccaea2cc3d0517e826
sakito <sakito21@gmail.com>
2019-06-17 18:07:53 +0900

    fix react-typescript/ok.tsx

--- test/fixtures/react-typescript/ok.tsx
+++ test/fixtures/react-typescript/ok.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import * as React from 'react';
 
 interface Props {
   name: string;
 }
 
-const Foo = ({name}: Props) => <p>Foo{name}</p>;
+const Foo:React.FC<Props> = ({name}) => <p>Foo{name}</p>;
 
 const Component = () => <Foo name="bar" />;
 export default Component;
