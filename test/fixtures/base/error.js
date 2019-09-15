351b73b3bff339ebd035c5526f5b6ada4968e2c9
Toru Kobayashi <koba0004@gmail.com>
2019-09-15 15:11:37 +0900

    feat: enable new rules(no-import-assign, default-param-last, prefer-regex-literals)
    BREAKING CHANGE: enable no-import-assign, default-param-last, prefer-regex-literals rules

--- test/fixtures/base/error.js
+++ test/fixtures/base/error.js
@@ -1,3 +1,5 @@
+import mod from './mod';
+
 var foo = {};
 var foo = {};
 const obj = {
@@ -9,6 +11,18 @@ const obj = {
 
 obj.a = obj.a;
 
+mod = 'mod';
+console.log(mod);
+
 (async () => {
   obj.a += await new Promise(async (r) => r(10));
 })();
+
+function fn(a, b = 'b', c) {
+  // noop
+}
+fn();
+
+const regexp = new RegExp('foo|bar');
+console.log(regexp);
+
