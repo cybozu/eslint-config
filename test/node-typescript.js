82c72e336d2e9d98237119a4970f1efdd5bcc2ea
saji-ryu <rsajiki@gmail.com>
2024-02-26 17:10:18 +0900

    feat: use eslint-plugin-n rules

--- test/node-typescript.js
+++ test/node-typescript.js
@@ -5,13 +5,13 @@ describe("node-typescript", () => {
   it("should get expected errors and warninigs", async () => {
     const result = await runLintWithFixtures(
       "node-typescript",
-      "presets/node-typescript-prettier.js",
+      "presets/node-typescript-prettier.js"
     );
     assert.deepStrictEqual(result, {
       "ok.ts": {},
       "sum.ts": {},
       "error.ts": {
-        errors: ["node/no-missing-import"],
+        errors: ["n/no-missing-import"],
       },
     });
   });
