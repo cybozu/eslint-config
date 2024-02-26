36748d9aed13f99adc77c5221c8e0a6f7738c7cc
saji-ryu <rsajiki@gmail.com>
2024-02-26 18:31:15 +0900

    fix: add tailing comma

--- test/node-typescript.js
+++ test/node-typescript.js
@@ -5,7 +5,7 @@ describe("node-typescript", () => {
   it("should get expected errors and warninigs", async () => {
     const result = await runLintWithFixtures(
       "node-typescript",
-      "presets/node-typescript-prettier.js"
+      "presets/node-typescript-prettier.js",
     );
     assert.deepStrictEqual(result, {
       "ok.ts": {},
