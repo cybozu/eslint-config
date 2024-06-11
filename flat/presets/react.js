39fa8259a8573840a2794ec14d0f6773adb4bb7f
Daiki Maekawa <k1tikurisu@gmail.com>
2024-06-11 11:25:38 +0900

    feat: add lib/base, lib/react and presets/react

---
+++ flat/presets/react.js
@@ -0,0 +1,7 @@
+const base = require("../lib/base.js");
+const react = require("../lib/react.js");
+
+/**
+ * @type { import("eslint").Linter.FlatConfig[] }
+ */
+module.exports = [...base(), ...react(["**/*.{jsx}"])];
