bc1aecf79dec8463147318a1e60fa2324828e210
Daiki Maekawa <k1tikurisu@gmail.com>
2024-06-12 14:55:21 +0900

    test: add test presets/react.js

---
+++ test/flat/react-test.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const react = require("../../flat/presets/react");
+const runLintWithFixturesFlat = require("../lib/runLintWithFixturesFlat");
+
+describe("react", () => {
+  it("should get expected errors and warnings", async () => {
+    // We use react presets in order to support ES2017 syntax
+    const result = await runLintWithFixturesFlat("react", react);
+
+    assert.deepStrictEqual(result, {
+      "ok.jsx": {},
+      "warning.jsx": {
+        warnings: ["react/jsx-no-useless-fragment", "react/jsx-indent"],
+      },
+    });
+  });
+});
