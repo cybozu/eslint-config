84cc7b41aee1f7d1e41f09b15c7b602157c033b1
saji-ryu <rsajiki@gmail.com>
2024-12-04 10:08:15 +0900

    fix: preset base test

--- test/flat/preset-base-test.js
+++ test/flat/preset-base-test.js
@@ -1,13 +1,13 @@
 const assert = require("assert");
-const basePreset = require("../../flat/presets/react-prettier");
-const base = require("../../flat/lib/prettier");
+const basePreset = require("../../flat/presets/base");
+const base = require("../../flat/lib/base");
 const runLintWithFixturesFlat = require("../lib/runLintWithFixturesFlat");
 
-describe("flat preset react-prettier", () => {
-  it("should be able to use react-prettier as well as lib/prettier", async () => {
+describe("flat preset base", () => {
+  it("should be able to use base as well as lib/base", async () => {
     assert.deepStrictEqual(
-      await runLintWithFixturesFlat("prettier", base()),
-      await runLintWithFixturesFlat("prettier", basePreset)
+      await runLintWithFixturesFlat("base", base()),
+      await runLintWithFixturesFlat("base", basePreset)
     );
   });
 });
