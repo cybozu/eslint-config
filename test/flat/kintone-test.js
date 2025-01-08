7d2d3503f6e12d00f2b685f9f55037407c488816
saji-ryu <rsajiki@gmail.com>
2025-01-08 11:39:43 +0900

    feat: rename lib/kintone to kitnone-es5 and add new lib/kintone

--- test/flat/kintone-test.js
+++ test/flat/kintone-test.js
@@ -3,16 +3,7 @@ const kintone = require("../../flat/lib/kintone");
 const runLintWithFixturesFlat = require("../lib/runLintWithFixturesFlat");
 
 describe("flat kintone", () => {
-  it("should get expected errors and warnings with kintone config", async () => {
-    const result = await runLintWithFixturesFlat("kintone", kintone());
-    assert.deepStrictEqual(result, {
-      "error.js": {
-        errors: ["strict", "strict"],
-      },
-      "ok.js": {},
-    });
-  });
-  it("should be able to use kintone-customize-es5 as well as lib/kintone", async () => {
+  it("should be able to use kintone-customize as well as lib/kintone", async () => {
     assert.deepStrictEqual(
       await runLintWithFixturesFlat("globals-kintone", kintone()),
       {
@@ -20,7 +11,7 @@ describe("flat kintone", () => {
         "error.js": {
           errors: ["no-undef"],
         },
-      },
+      }
     );
   });
 });
