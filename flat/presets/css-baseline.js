42628f6f720d2b2ac7d90a3707a3c9bf170a88f2
sakura-adachi <sakura-adachi@cybozu.co.jp>
2025-07-08 11:51:11 +0900

    add @eslint/css & baseline rules

---
+++ flat/presets/css-baseline.js
@@ -0,0 +1,9 @@
+const cssBaseline = require("../lib/css-baseline.js");
+
+/**
+ * @type { import("eslint").Linter.Config[] }
+ */
+module.exports = [
+  { files: ["**/*.css"] },
+  ...cssBaseline(),
+];
---
+++ test/flat/preset-css-baseline-test.js
@@ -0,0 +1,16 @@
+const assert = require("assert");
+const runLintWithFixturesFlat = require("../lib/runLintWithFixturesFlat");
+const cssBaseline = require("../../flat/presets/css-baseline");
+
+describe("flat preset css-baseline", () => {
+  it("should get expected errors and warnings", async () => {
+    const result = await runLintWithFixturesFlat("css-baseline", cssBaseline);
+    assert.deepStrictEqual(result, {
+      "ok.css": {},
+      "error.css": {
+        errors: ["css/no-duplicate-imports"],
+        warnings: ["css/use-baseline", "css/use-baseline", "css/use-baseline", "css/use-baseline"],
+      },
+    });
+  });
+});
