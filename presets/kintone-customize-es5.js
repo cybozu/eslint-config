643f6da566455723d9f59c7a62ced4d194144cdf
sajikix <rsajiki@gmail.com>
2026-04-17 10:56:56 +0900

    fix: switch to default import for kintoneGlobals in multiple files

--- presets/kintone-customize-es5.js
+++ presets/kintone-customize-es5.js
@@ -1,6 +1,6 @@
 import { es5 } from "../lib/es5.js";
 import { kintoneEs5 } from "../lib/kintone-es5.js";
-import { kintoneGlobals } from "../globals/kintone.js";
+import kintoneGlobals from "../globals/kintone.js";
 import { attachFilesPropForConfig } from "../utils/attachFilesPropForConfig.js";
 
 /**
