6b7fbae6308d0a13ed77058f93f43e6eaa52a480
sajikix <rsajiki@gmail.com>
2026-07-02 16:04:08 +0900

    refactor: replace hand-ported jsx formatting rules with @stylistic rules

--- plugins/react/index.js
+++ plugins/react/index.js
@@ -8,13 +8,8 @@ import noDangerWithChildren from "./rules/no-danger-with-children.js";
 import jsxFilenameExtension from "./rules/jsx-filename-extension.js";
 import voidDomElementsNoChildren from "./rules/void-dom-elements-no-children.js";
 import jsxBooleanValue from "./rules/jsx-boolean-value.js";
-import jsxPascalCase from "./rules/jsx-pascal-case.js";
-import jsxCurlyBracePresence from "./rules/jsx-curly-brace-presence.js";
 import jsxNoUselessFragment from "./rules/jsx-no-useless-fragment.js";
 import jsxNoBind from "./rules/jsx-no-bind.js";
-import selfClosingComp from "./rules/self-closing-comp.js";
-import jsxFirstPropNewLine from "./rules/jsx-first-prop-new-line.js";
-import jsxMaxPropsPerLine from "./rules/jsx-max-props-per-line.js";
 import jsxNoUndef from "./rules/jsx-no-undef.js";
 import noUnescapedEntities from "./rules/no-unescaped-entities.js";
 import noMultiComp from "./rules/no-multi-comp.js";
@@ -35,13 +30,8 @@ const reactPlugin = {
     "jsx-filename-extension": jsxFilenameExtension,
     "void-dom-elements-no-children": voidDomElementsNoChildren,
     "jsx-boolean-value": jsxBooleanValue,
-    "jsx-pascal-case": jsxPascalCase,
-    "jsx-curly-brace-presence": jsxCurlyBracePresence,
     "jsx-no-useless-fragment": jsxNoUselessFragment,
     "jsx-no-bind": jsxNoBind,
-    "self-closing-comp": selfClosingComp,
-    "jsx-first-prop-new-line": jsxFirstPropNewLine,
-    "jsx-max-props-per-line": jsxMaxPropsPerLine,
     "jsx-no-undef": jsxNoUndef,
     "no-unescaped-entities": noUnescapedEntities,
     "no-multi-comp": noMultiComp,
